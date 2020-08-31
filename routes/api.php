0bcc32d04d0bc7dfd209c321202624fc9c9c8a52
semih can <smhcnx@gmail.com>
2020-09-01 00:32:22 +0300

    Laravel Password Auth Api Created

--- routes/api.php
+++ routes/api.php
@@ -14,16 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+    Route::get('/home', 'HomeController@index')->name('home');
 });
-
-/*
-Route::get('country','Country\CountryController@country');
-Route::get('country/{id}','Country\CountryController@countryById');
-Route::post('country','Country\CountryController@countrySave');
-Route::put('country/{id}','Country\CountryController@countryUpdate');
-Route::delete('country/{country}','Country\CountryController@countryDelete');
-*/
-
-Route::apiResource('country','Country\Country');
